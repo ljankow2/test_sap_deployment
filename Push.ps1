ab529c1b6781976e44032d5e9ba45fb2058fa896
ljankow2 <51783305+ljankow2@users.noreply.github.com>
2019-06-27 11:33:16 +0200

    Update Push.ps1

--- Push.ps1
+++ Push.ps1
@@ -1,7 +1,7 @@
 $Subs_Name = "Subs_TEST_ARM"
 $Location = "westeurope"
 $Dep_Name = "go"
-$Template_URL = "https://github.com/ljankow2/test_sap_deployment/blob/master/Resurce_Group-01-02--Creating_in_loops.json"
+$Template_URL = "https://raw.githubusercontent.com/ljankow2/test_sap_deployment/master/Resurce_Group-01-02--Creating_in_loops.json?token=AMLCNCJIZ2V4DNFUGBZBQFS5CSGXE"
 
 # Select-AzSubscription -SubscriptionName $Subs_Name
 
